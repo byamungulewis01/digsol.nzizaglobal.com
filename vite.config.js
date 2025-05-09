a3d5274f082f29d851f087dd953c21ffc5de0763
byamungulewis01 <ntwarilebon09@gmail.com>
2025-05-09 13:41:44 +0200

    correct git changes

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
-    react(),
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|webp|svg)$/i,
-      includePublic: true,
-      logStats: true,
-      ansiColors: true,
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: 'preset-default',
-            params: {
-              overrides: {
-                cleanupNumericValues: false,
-                removeViewBox: false,
-              },
-            },
-          },
-        ],
-      },
-      png: {
-        quality: 80,
-      },
-      jpeg: {
-        quality: 80,
-      },
-      jpg: {
-        quality: 80,
-      },
-      webp: {
-        lossless: true,
-      },
-    }),
+    react()
   ],
   resolve: {
     alias: {
